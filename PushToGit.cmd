f45685f3d697466c871414acdf094a51e0181b15
Prince Kumar <kpm563@gmail.com>
2018-03-15 12:22:04 +0530

    NUnit Test and How to implement in programs.

---
+++ PushToGit.cmd
@@ -0,0 +1,11 @@
+echo off
+echo Start initialization to git :-
+git init
+echo Adding current directory to git :-
+git add .
+echo Committing to git :-
+git commit -m "NUnit Test and How to implement in programs."
+echo Pushing to git :-
+git push -u origin master
+echo Pushing Done...!!
+exit
