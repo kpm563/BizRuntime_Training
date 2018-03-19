1bf99f8ad3be6509bc046932c65d85daedafd45e
Prince Kumar <kpm563@gmail.com>
2018-03-19 19:39:52 +0530

    This is implementation of network programming in c#

---
+++ Network_Programming/ShowIP.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+
+namespace NetworkProject
+{
+	class ShowIP
+	{		
+		static void Main(string[] args)
+		{
+			string name = (args.Length < 1) ? Dns.GetHostName() : args[0];
+			try
+			{
+				IPAddress[] iPAddresses = Dns.Resolve(name).AddressList;
+				foreach (IPAddress address in iPAddresses)
+					Console.WriteLine("{0}/{1}", name, address);
+			}
+			catch (Exception e) {
+				Console.WriteLine(e.Message);
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
