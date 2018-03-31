b70f35773d04a57417b705c0aeb29d7c545d4aca
Prince Kumar <kpm563@gmail.com>
2018-03-31 20:19:47 +0530

    This is having implementation of MVC using ASP .net core.

---
+++ ASP .net core/CoreMVC_Project/CoreMVC_Project/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace CoreMVC_Project.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Department { get; set; }
+        public string City { get; set; }
+
+    }
+}
