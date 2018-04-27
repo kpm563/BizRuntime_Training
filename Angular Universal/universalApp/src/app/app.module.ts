0a94de0760356806ff0a19c523ea61d4ebbd677f
Prince Kumar <kpm563@gmail.com>
2018-04-27 10:25:09 +0530

    This is having implementation of angular universal.

---
+++ Angular Universal/universalApp/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {PostComponent} from './post/post.component';
+
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
